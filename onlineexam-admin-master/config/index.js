ef70e424bc0ed78db1e2a459d66c3f2541e65d05
wuhufly <1115499597@qq.com>
2022-10-27 00:22:36 +0800

    修改单选bug问题

--- onlineexam-admin-master/config/index.js
+++ onlineexam-admin-master/config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {
       '/api': {
         // 匹配所有以 '/api'开头的请求路径
-        target: 'http://49.234.12.39:8080', // 代理目标的基础路径
+        // target: 'http://49.234.12.39:8080', // 代理目标的基础路径
+        target: 'http://localhost:8080', // 代理目标的基础路径
         // secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true // 支持跨域
         // pathRewrite: { // 重写路径: 去掉路径中开头的'/api'
